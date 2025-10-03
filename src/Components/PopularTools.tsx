02faf153602e18798ed610f2d3549610d147f0ea
Sylva-Egb <romaricegbewole@gmail.com>
2025-10-04 00:59:23 +0100

    Section Most Popular TOols débutée

---
+++ src/Components/PopularTools.tsx
@@ -0,0 +1,13 @@
+function PopularTools() {
+    return (
+        <div className="bg-black text-white">
+            <div className="flex justify-center flex-col items-center">
+                <h1 className="text-4xl mb-4 font-bold">Most Popular Tools</h1>
+                <span>Tools for the best Designers and Developers</span>
+                <span>most popularly used in the world</span>
+            </div>
+        </div>
+    )
+}
+
+export default PopularTools
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import PopularTools from "./Components/PopularTools";
 
 function Home() {
     return (
         <div>
             <Header />
             <Hero />
+            <PopularTools />
         </div>
     )
 }
